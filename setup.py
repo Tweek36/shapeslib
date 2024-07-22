140b1b62d9705374a6d0b4e04050085c3d8ff2cd
tweek36 <sowakov.yura@gmail.com>
2024-07-22 23:34:28 +0300

    fix

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='shapeslib',
-    version='1.0.0',
+    name="shapeslib",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[],
-    author='Tweek36',
-    author_email='sowakov.yura@gmail.com',
-    description='Создаёт фигуры',
-    url='https://github.com/Tweek36/shapeslib',
+    test_suite='tests',
 )
